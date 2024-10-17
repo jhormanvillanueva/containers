ea0bfee72725d76a685d42f1829778841bdd4c02
jhorman villanueva <jhorman.villanueva@unad.edu.co>
2024-10-17 10:16:28 +0000

    add container db

--- python-container/app.py
+++ python-container/app.py
@@ -1,10 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template
+import mysql.connector
+
 
 app = Flask(__name__)
 
+
+
 @app.route('/')
-def index():
-    return "Hello World from Container"
+def home():
+	return render_template("index.html")
+
+@app.route('/libros')
+def libros():
+    conn = mysql.connector.connect(
+        host="mariadb-container",
+        user="root",
+        password="Test!2024",
+        database="books_db")
+    
+    c = conn.cursor()
+    c.execute("SELECT * FROM Books")
+    books = c.fetchall()
+    conn.close()
+    return render_template("libros.html", books=books)
 
 if __name__=='__main__':
-    app.run(host='0.0.0.0', debug=True)
+	app.run(host='0.0.0.0', debug=True)
